fd3bb61d655b688a6aaf55edefceed6d021ea221
saifShaikh404 <shaikhsaif78673@gmail.com>
2024-04-04 23:30:27 +0530

    first
    UI related stuff, Material App, Scaffold, StatelessWidget, Text, TextInput, Column, Padding, Container, Button Widgets are covered...

---
+++ lib/home_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    print('running');
+    TextEditingController textController = TextEditingController();
+    double initVal = 0;
+
+    final textFieldBorder = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(15),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Text(
+                initVal.toString(),
+                style: const TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: textController,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.monetization_on),
+                  prefixIconColor: Colors.black,
+                  hintText: 'Please Enter a Value',
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: textFieldBorder,
+                  enabledBorder: textFieldBorder,
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (kDebugMode) {
+                      // run when app is in debug mode
+                      print('Button Clicked');
+                      initVal = double.parse(textController.text);
+                      build(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                  child: const Text(
+                    'Click',
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
